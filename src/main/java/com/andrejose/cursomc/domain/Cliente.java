6e81153dfe0399ee5b68f5afd95b250bd5fa64e6
Andre Manacaze <andrewsjosemanacaze@gmail.com>
2022-06-30 18:11:19 +0200

    Implementada a remoção de endereços em cascata do cliente

--- src/main/java/com/andrejose/cursomc/domain/Cliente.java
+++ src/main/java/com/andrejose/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -29,7 +30,7 @@ public class Cliente implements Serializable{
 	private String cpf;
 	private Integer tipoCliente; //Armazenar internamente o numero inteiro
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL) //Possibilitar apagar endereços do cliente em cascata
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	
